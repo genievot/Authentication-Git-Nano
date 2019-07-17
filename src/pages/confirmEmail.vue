9cddc01b0b29f5a05f1eb3a05cfe02b9b19134e7
Genievot <40867747+genievot@users.noreply.github.com>
2019-07-17 09:51:53 +0530

    removing some code
    Removing useless code now.

--- src/pages/confirmEmail.vue
+++ src/pages/confirmEmail.vue
@@ -34,54 +34,6 @@ export default {
       })
       let emailPassClient = this.$stitchClient.auth.getProviderClient(this.$userPasswordAuthProviderClient.factory)
       emailPassClient.confirmUser(this.$route.query.token, this.$route.query.tokenId).then(() => {
-        this.$axios.get(this.$backEnd + '/setupUserConfirmaion', { // AXIOS CALL
-          params: {
-            node_id: this.$q.localStorage.getItem('userLogged').nodeId,
-            emailUsed: this.$q.localStorage.getItem('selectedEmail')
-          }
-        }).then((response) => {
-          // console.log(this.$q.localStorage.getItem('selectedEmail'))
-          // console.log(response)
-          this.$q.localStorage.set('userSecDetails', response.data)
-          // console.log(this.$q.localStorage.getItem('authCredentials'))
-          this.$stitchClient.auth.loginWithCredential(this.$q.localStorage.getItem('authCredentials')).then((authedUser) => {
-            let moreData = {
-              user_auth_id: authedUser.id
-            }
-            let dataToInsert = { ...response.data, ...moreData }
-            let publicDataToInsert = { account: response.data.account, userName: response.data.user_name, avatar_url: response.data.avatar_url, user_auth_id: authedUser.id }
-            try {
-              this.$db.collection('userInfo').insertOne(dataToInsert, (err, res) => {
-                if (err) {
-                  console.log(err)
-                } else {
-                  this.$axios.get(this.$backEnd + '/remove/sensData', { // AXIOS CALL
-                    params: {
-                      node_id: this.$q.localStorage.getItem('userLogged').nodeId
-                    }
-                  }).then((res) => {
-                    console.log('Wallet Not in server anymore...')
-                  }).catch(err => {
-                    console.log(err)
-                  })
-                }
-              })
-              this.$db.collection('publicUserInfo').insertOne(publicDataToInsert)
-            } catch (e) {
-              console.log(e)
-            }
-          }).catch(err => {
-            console.error(err)
-          })
-        }).catch((err) => {
-          console.log(err)
-          this.$q.notify({
-            color: 'warning',
-            message: 'A problem while saving data to database, But your account is fully functional and working, YAY!',
-            timeout: 25000,
-            icon: 'warning'
-          })
-        })
         console.log('Done')
         this.$q.loading.hide()
         this.$q.notify({
